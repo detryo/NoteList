0d9091669e3368982c03aab50a42a228f7d80ed1
Chris <chris.sedano.arenas@gmail.com>
2020-02-14 15:30:31 +0000

    Implement UI
    implement CollectionView Embed in Navigation, and implement TableView with navigation bar, implement NotesTableViewController.swift and CategoriesViewController.swift

---
+++ NotesOnTheGo/Controllers/CategoriesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesOnTheGo
+//
+//  Created by Cristian Sedano Arenas on 14/02/2020.
+//  Copyright © 2020 Cristian Sedano Arenas. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
